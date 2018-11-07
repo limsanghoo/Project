5da89aa452eda84df5275862f7b9fc318d1cd8ac
junseok <junseok@gmail.com>
2018-11-07 21:01:07 +0900

    mapper제작중

---
+++ 04.Model2MVCShop(Business Logic,MyBatis Spring)/src/main/java/com/model2/mvc/service/purchase/impl/PurchaseServiceImpl.java
@@ -0,0 +1,53 @@
+package com.model2.mvc.service.purchase.impl;
+
+import java.util.Map;
+
+import com.model2.mvc.common.Search;
+import com.model2.mvc.service.domain.Purchase;
+import com.model2.mvc.service.purchase.PurchaseService;
+
+public class PurchaseServiceImpl implements PurchaseService {
+
+	@Override
+	public void addPurchase(Purchase purchase) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Purchase getPurchase(int tranNo) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Purchase getPurchase2(int ProdNo) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, Object> getPurchaseList(Search search, String buyerId) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, Object> getSaleList(Search search) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updatePurchase(Purchase purchase) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateTranCode(Purchase purchase) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
